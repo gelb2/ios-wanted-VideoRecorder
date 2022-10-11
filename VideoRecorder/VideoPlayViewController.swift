df6e8e96116f921ad55e00bd0ae68885f021237d
hemg2 <hemg2@naver.com>
2022-10-11 23:19:40 +0900

    네비영역 파일 재생

--- VideoRecorder/VideoPlayViewController.swift
+++ VideoRecorder/VideoPlayViewController.swift
@@ -11,28 +11,40 @@ import AVFoundation
 
 
 class VideoPlayViewController: UIViewController, AVPlayerViewControllerDelegate {
-
+    
     var playerLayer = AVPlayerViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
+    
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        playVideo()
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "Sim", ofType: "mp4")!))
+        
+        let vc = AVPlayerViewController()
+        vc.player = player
+        present(vc, animated: true)
     }
-
-    func playVideo() {
-        guard let videoURl = URL(string:  "https://wolverine.raywenderlich.com/content/ios/tutorials/video_streaming/foxVillage.mp4") else { return }
-        let player = AVPlayer(url: videoURl)
-        playerLayer = AVPlayerViewController()
-        playerLayer.player = player
-        playerLayer.allowsPictureInPicturePlayback = true
-        playerLayer.delegate = self
-        playerLayer.player?.play()
-
-        self.present(playerLayer, animated: true, completion: nil)
-    }
-
 }
+
+
+
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        playVideo()
+//    }
+//
+//    func playVideo() {
+//        guard let videoURl = URL(string:  "https://wolverine.raywenderlich.com/content/ios/tutorials/video_streaming/foxVillage.mp4") else { return }
+//        let player = AVPlayer(url: videoURl)
+//        playerLayer = AVPlayerViewController()
+//        playerLayer.player = player
+//        playerLayer.allowsPictureInPicturePlayback = true
+//        playerLayer.delegate = self
+//        playerLayer.player?.play()
+//
+//        self.present(playerLayer, animated: true, completion: nil)
+//    }
