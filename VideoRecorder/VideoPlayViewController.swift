d66c4c51df9224047f4395dc5e3b7f375314d55e
hemg2 <hemg2@naver.com>
2022-10-12 00:31:20 +0900

    feat: Navigation 추가

--- VideoRecorder/VideoPlayViewController.swift
+++ VideoRecorder/VideoPlayViewController.swift
@@ -8,43 +8,62 @@
 import UIKit
 import AVKit
 import AVFoundation
+import Photos
 
 
 class VideoPlayViewController: UIViewController, AVPlayerViewControllerDelegate {
     
     var playerLayer = AVPlayerViewController()
+//    var allPhotos: PHFetchResult<PHAsset>?
+    
+    lazy var rightNavButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(title: "⌾", style: .plain, target: self, action: #selector(add))
+        return button
+    }()
+    lazy var leftNavButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(title: "〈", style: .plain, target: self, action: #selector(back))
+        return button
+    }()
+    @objc func add(_ sender: Any) {
+    }
+    @objc func back(_ sender: Any) {
+        //        let firstView = firstViewController() // 첫번째 화면 푸시
+        //        self.navigationController?.pushViewController(firstView, animated: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        self.navigationItem.rightBarButtonItem = self.rightNavButton
+        self.navigationItem.leftBarButtonItem = self.leftNavButton
+        title = "네비바 제목"
+        playVideo()
+//        self.allPhotos = PHAsset.fetchAssets(with: nil)
         
+//        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "Sim", ofType: "mp4")!))
+//
+//        let vc = AVPlayerViewController()
+//        vc.player = player
+//        present(vc, animated: true)
     }
     
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "Sim", ofType: "mp4")!))
-        
-        let vc = AVPlayerViewController()
-        vc.player = player
-        present(vc, animated: true)
+    func playVideo() {
+        guard let videoURl = URL(string:  "https://wolverine.raywenderlich.com/content/ios/tutorials/video_streaming/foxVillage.mp4") else { return }
+        let player = AVPlayer(url: videoURl)
+        playerLayer = AVPlayerViewController()
+        playerLayer.player = player
+        playerLayer.allowsPictureInPicturePlayback = true
+        playerLayer.delegate = self
+        playerLayer.player?.play()
+
+        self.present(playerLayer, animated: true, completion: nil)
     }
+
+    
 }
 
 
 
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        playVideo()
-//    }
-//
-//    func playVideo() {
-//        guard let videoURl = URL(string:  "https://wolverine.raywenderlich.com/content/ios/tutorials/video_streaming/foxVillage.mp4") else { return }
-//        let player = AVPlayer(url: videoURl)
-//        playerLayer = AVPlayerViewController()
-//        playerLayer.player = player
-//        playerLayer.allowsPictureInPicturePlayback = true
-//        playerLayer.delegate = self
-//        playerLayer.player?.play()
-//
-//        self.present(playerLayer, animated: true, completion: nil)
-//    }
+
+
+
