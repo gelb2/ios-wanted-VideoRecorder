5666d215de17480288b8151980fcedcf0b6cff4b
hemg2 <hemg2@naver.com>
2022-10-12 14:08:22 +0900

    Revert "Merge branch 'Develop' into VideoRecorder"
    This reverts commit 7c07ca4d3f3cfddc051ed12636845650d1d23463, reversing
    changes made to 790b6cfd11ce154229d1e89a0d0337b2b2e6ace6.

--- VideoRecorder/SceneDelegate.swift
+++ VideoRecorder/SceneDelegate.swift
@@ -7,23 +7,22 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate, SceneDelegateRoutable {
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    var windowScene: UIWindowScene?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        self.windowScene = (scene as? UIWindowScene)
-        
-        let httpClient = HTTPClient()
-        let model = FirstModel(repository: Repository(httpClient: httpClient))
-        let sceneContext = SceneContext(dependency: model)
-        route(to: .main(.firstViewController(context: sceneContext)))
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        let navigationController = UINavigationController(rootViewController: VideoPlayViewController())
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- VideoRecorder/Utilities/Entity.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Entity.swift
-//  VideoRecorder
-//
-//  Created by sokol on 2022/10/11.
-//
-
-import Foundation
